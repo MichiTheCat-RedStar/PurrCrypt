3a689af165b879e4d40ffb2174417548a2b91f7c
Michi The Cat <mrzombsoriginal@gmail.com>
2026-03-19 16:21:13 +0300

    My Safety

--- PurrCrypt.py
+++ PurrCrypt.py
@@ -1,3 +1,8 @@
+# Данный код распространяется по MIT лицензии автром MichiTheCat
+# Не видитесь на проекты, выдающие себя за оригинал или подобное
+# Оригинал кода https://github.com/MichiTheCat-RedStar/PurrCrypt
+
+
 from random import seed, getrandbits
 
 def purr_encode(*text:str) -> str:
